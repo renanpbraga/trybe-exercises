f84a089abafb3adae464894803162b16c03945ca
renanpbraga <renanpbraga@gmail.com>
2021-02-25 19:30:23 -0300

    resolve o exercise7

--- modulo1/bloco4/dia1/exercise7.js
+++ modulo1/bloco4/dia1/exercise7.js
@@ -1,7 +1,15 @@
-let nota = 92;
+let nota = 40;
 
-switch (nota) {
-    case (nota >= 90):
-        console.log ("vertical");
-        break;
-}
+if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80 && nota < 90) {
+    console.log ("B");
+    } else if (nota >= 70 && nota < 80) {
+        console.log("C");
+            } else if (nota >=60 && nota < 80) {
+                console.log ("D");
+                    } else if (nota >= 50 && nota < 60) {
+                        console.log("E")
+                            } else if (nota < 50) {
+                                console.log ("F");
+                            }
