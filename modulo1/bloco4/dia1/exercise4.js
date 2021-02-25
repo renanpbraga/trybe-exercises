8c58264a3a1abce404ba4286cc39d93011b19b8f
renanpbraga <renanpbraga@gmail.com>
2021-02-25 19:05:50 -0300

    adiciona exercise4

---
+++ modulo1/bloco4/dia1/exercise4.js
@@ -0,0 +1,14 @@
+let valor ="xablau";
+
+switch (valor) {
+    case valor= "positivo":
+        console.log("positive");
+        break;
+
+        case valor= "negativo":
+            console.log("negative");
+            break;
+
+            default:
+                console.log("zero");
+}
