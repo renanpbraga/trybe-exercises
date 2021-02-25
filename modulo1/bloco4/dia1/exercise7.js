f47f725f880eb8ec10f86f51b9b72010976cd8ec
renanpbraga <renanpbraga@gmail.com>
2021-02-25 19:46:27 -0300

    corrige exercise7

--- modulo1/bloco4/dia1/exercise7.js
+++ modulo1/bloco4/dia1/exercise7.js
@@ -1,6 +1,6 @@
-let nota = 40;
+let nota = 110;
 
-if (nota >= 90) {
+if (nota <= 100 && nota >=90) {
     console.log("A");
 } else if (nota >= 80 && nota < 90) {
     console.log ("B");
@@ -10,6 +10,10 @@ if (nota >= 90) {
                 console.log ("D");
                     } else if (nota >= 50 && nota < 60) {
                         console.log("E")
-                            } else if (nota < 50) {
+                            } else if (nota < 50 && nota > 0) {
                                 console.log ("F");
-                            }
+                                    } else if (nota <0) {
+                                        console.log ("Escolha um valor entre 0 e 100")
+                                        } else {
+                                            console.log("escolha um valor entre 0 e 100")
+                                        }
