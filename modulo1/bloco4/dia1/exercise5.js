cc1342c5a7f1148c47305c0bea13fdae21a02b86
renanpbraga <renanpbraga@gmail.com>
2021-02-25 16:34:45 -0300

    adiciona o exercise5

---
+++ modulo1/bloco4/dia1/exercise5.js
@@ -0,0 +1,9 @@
+let anguloA =60;
+let anguloB =60;
+let anguloC =60;
+
+if (anguloA + anguloB + anguloC == 180) {
+    console.log("é um triangulo");
+} else {
+        console.log("não é um triangulo");
+    }
