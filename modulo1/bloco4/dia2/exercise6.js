e4612120a5b0af748d8c205e85e110db63de0fd1
renanpbraga <renanpbraga@gmail.com>
2021-02-26 19:37:25 -0300

    adiciona exercise6

---
+++ modulo1/bloco4/dia2/exercise6.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let temImpar = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] % 2 != 0) {
+        temImpar++
+    }
+}
+    console.log(temImpar);
