b0be1d4938bb653728bdb7f2d2af4f980466bccd
renanpbraga <renanpbraga@gmail.com>
2021-02-26 12:31:43 -0300

    adiciona exercise9

--- modulo1/bloco4/dia1/exercise3.js
+++ modulo1/bloco4/dia1/exercise3.js
@@ -7,5 +7,5 @@ if (num1 > num2 && num1 > num3) {
 } else if (num2 > num3) {
     console.log ("10 é o maior número!");
 } else {
-    console.log ("15 é o maior número!")
+    console.log ("15 é o maior número")
 }
--- modulo1/bloco4/dia1/exercise9.js
+++ modulo1/bloco4/dia1/exercise9.js
@@ -1,7 +1,7 @@
 let num1 = 5;
 let num2 = 15;
 let num3 = 101;
-if ((num1 % 2 == 0) || (num2 % 2 == 0) || (num3 % 2 == 0)){
+if ((num1 % 2 != 0) || (num2 % 2 != 0) || (num3 % 2 != 0)){
   console.log(true)
 }
 else {
