3d984c80053d70b4cf7ac4e15cf0d580abbfda51
renanpbraga <renanpbraga@gmail.com>
2021-02-26 18:21:23 -0300

    corrige exercise4

--- modulo1/bloco4/dia2/exercise4.js
+++ modulo1/bloco4/dia2/exercise4.js
@@ -1,13 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
 
-for (let index=0; index < numbers.length; index ++) {
-    (resultado += numbers[index]);
+for (let index = 0; index < numbers.length; index++) {
+  resultado += numbers[index];
 }
-    let media = (resultado/numbers.length);
-    console.log (media);
-if (media >= 20) {
-    console.log ("valor maior que 20");
-}   else {
-        console.log("valor menor que 20")
-    }
+
+resultado = resultado / numbers.length;
+
+if (resultado > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
