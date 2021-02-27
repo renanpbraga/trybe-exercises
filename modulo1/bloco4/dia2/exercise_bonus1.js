336aa4f9f9a9511171eefa5a7cffb986a3b80c5a
renanpbraga <renanpbraga@gmail.com>
2021-02-26 23:45:49 -0300

    corrige exercise_bonus1

--- modulo1/bloco4/dia2/exercise_bonus1.js
+++ modulo1/bloco4/dia2/exercise_bonus1.js
@@ -1,12 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let numeroDaFrente = 1; numeroDaFrente < numbers.length; numeroDaFrente++) {
-  for (let numeroDeTras = 0; numeroDeTras < numeroDaFrente; numeroDeTras++) {
-    if (numbers[numeroDaFrente] < numbers[numeroDeTras]) {
-      let posicao = numbers[numeroDaFrente];
+for (let frontNum = 1; frontNum < numbers.length; frontNum++) {
+  for (let backwNum = 0; backwNum < frontNum; backwNum++) {
+    if (numbers[frontNum] < numbers[backwNum]) {
+      let position = numbers[frontNum];
 
-      numbers[numeroDaFrente] = numbers[numeroDeTras];
-      numbers[numeroDeTras] = posicao;
+      numbers[frontNum] = numbers[backwNum];
+      numbers[backwNum] = position;
     }
   }
 }
