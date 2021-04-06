74d44720028b7a4f28d9339b4ada3daec73df5cc
renanpbraga <renanpbraga@gmail.com>
2021-04-06 18:31:21 -0300

    adiciona exercise5 errado

--- modulo1/bloco8/exercises/higher-order-functions-do-javascript-es6_3/exercise5.js
+++ modulo1/bloco8/exercises/higher-order-functions-do-javascript-es6_3/exercise5.js
@@ -64,30 +64,17 @@ const books = [
 ];
 
 const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954,
-  },
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
 ];
 
-function oldBooks() {
-  return books.filter((book) => (book.releaseYear < 1961)).sort((a, b) => a.releaseYear - b.releaseYear);
+function fantasyOrScienceFictionAuthors() {
+    const findGenre = books.filter((book) => {
+        return (book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+    }).map((book) => (
+        book.author.name
+    ));
 }
-assert.deepStrictEqual(oldBooks(), expectedResult);
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
