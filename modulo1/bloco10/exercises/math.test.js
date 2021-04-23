f9aa3fd7447449c222e997d5354ae4e80c9184ce
renanpbraga <renanpbraga@gmail.com>
2021-04-22 21:40:26 -0300

    exercicio de fixacao

--- modulo1/bloco10/exercises/math.test.js
+++ modulo1/bloco10/exercises/math.test.js
@@ -5,9 +5,28 @@ jest.mock('./math'); // mocka todo o arquivo math.
 describe('testa funções de Math', () => { // cria o ambiente de testes
 
     test('testa a função subtrair', () => { // testa a função especifica
-    
+        expect.assertions(1)
         subtrair.mockImplementation((a, b) => a - b); // reimplementa a função a ser testada
-        expect(subtrair(2, 1)).toBe(1); // passa a função testada e os parâmetros e o valor a retornar
-    
+        subtrair(); //chama a função por conta do toHaveBennCalled
+        expect(subtrair).toHaveBeenCalled(); // passa a função testada e os parâmetros e o valor a retornar
     });
+
+    test('testa a função multiplicar', () => {
+        expect.assertions(2);
+        multiplicar.mockImplementation((a, b) => a * b);
+        multiplicar();
+        expect(multiplicar).toHaveBeenCalled();
+        expect(multiplicar(2, 5)).toBe(10);
+    });
+    
+    test('testa a função somar', () => {
+        expect.assertions(3);
+        somar.mockImplementation((a, b) => a + b);
+        somar();
+        expect(somar).toHaveBeenCalled();
+        expect(somar(2, 5)).toBe(7);
+        expect(somar).toHaveBeenCalledWith(2, 5);
+    });
+
+    test()
 });
