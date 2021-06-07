f3b98f0ecaa7c7d1de4f3dc3d7427fc2e33776e3
renanpbraga <renanpbraga@gmail.com>
2021-06-07 18:53:34 -0300

    adiciona exercise counter-app-test

---
+++ modulo2/bloco16/16.5/counter-app/src/App.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, cleanup, screen } from '@testing-library/react';
+import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+import userEvent from '@testing-library/user-event';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+
+  test('testa se um click altera o valor do estado', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const button = queryByText(/clique aqui/i)
+    userEvent.click(button)
+
+    expect(queryByText('1')).toBeInTheDocument();
+  });
+});
